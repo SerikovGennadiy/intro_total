ca65cbe6cf002b8556907f5a1162698089100540
GennadiySerikov <serikov.gen@yandex.ru>
2022-09-19 23:20:07 +0300

    feat: app first issue ready

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var words = new string[] {
+    "Hello", "2", "world", ":-)"
+};
+
+ShowWords(GetWordsLessThen(words, 3));
+
+
+int HowManyWordsLessThen(string[] words, int wordLength)
+{
+    int result = 0;
+    foreach(var word in words)
+    {
+        if(word.Length <= wordLength)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+string[] GetWordsLessThen(string[] words,  int wordLength) 
+{
+   string[] result = new string[HowManyWordsLessThen(words, wordLength)];
+   
+   int i = 0;
+   foreach(var word in words)
+    {
+        if(word.Length <= wordLength)
+        {
+            result[i++] = word;
+        }
+    }
+    return result;
+}
+
+void ShowWords(string[] words) 
+{
+    foreach(var word in words) 
+    {
+        System.Console.WriteLine(word);
+    }
+}
+
+// words
+//     .Where(word => word.Length <= 3)
+//     .ToList()
+//     .ForEach(word => Console.WriteLine(word));
